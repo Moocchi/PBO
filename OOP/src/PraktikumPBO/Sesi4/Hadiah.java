dc395b764585b9b8deb561b1765b65c56bca2f73
Moocchi <muhammadjarez@gmail.com>
2024-10-09 10:32:45 +0700

    update sesi 4

--- OOP/src/PraktikumPBO/Sesi4/Hadiah.java
+++ OOP/src/PraktikumPBO/Sesi4/Hadiah.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Hadiah {
     public static void main(String[] args) {
         //membuat variabel belanja dan scanner
-        int belanja = 0;
-        Scanner scan = new Scanner(System.in);
+        int belanja;
+    try (Scanner scan = new Scanner(System.in)) {
         //mengambil input
         System.out.print("Total Belanja : Rp ");
         belanja = scan.nextInt();
         //Cek apakah dia belanja di atas 100.000
         if (belanja > 100000) {
             System.out.println("Selamat, Anda mendapatkan hadiah!");
+            System.out.println("Hadiahnya adalah : Action Figure Iqbal Kuda");
         }
         System.out.println("Terima Kasih...");
-    scan.close();
+    }
     }
 }
--- OOP/src/PraktikumPBO/Sesi4/LampuLaluLintas.java
+++ OOP/src/PraktikumPBO/Sesi4/LampuLaluLintas.java
@@ -6,18 +6,17 @@ public class LampuLaluLintas {
     public static void main(String[] args) {
         //Membuat Variabel dan Scanner
         String lampu;
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            //Mengambil Input
+            System.out.print("Inputkan Nama Warna: ");
+            lampu = scan.nextLine();
 
-        //Mengambil Input
-        System.out.print("Inputkan Nama Warna: ");
-        lampu = scan.nextLine();
-
-        switch (lampu) {
-            case "merah" -> System.out.println("Lampu merah, berhenti!");
-            case "kuning" -> System.out.println("Lampu kuning, harap hati-hati!");
-            case "hijau" -> System.out.println("Lampu hijau, Silakan Jalan!");
+            switch (lampu) {
+                case "merah" -> System.out.println("Lampu merah, berhenti!");
+                case "kuning" -> System.out.println("Lampu kuning, harap hati-hati!");
+                case "hijau" -> System.out.println("Lampu hijau, Silakan Jalan!");
                 default -> System.out.println("Warna Lampu Salah");
             }
-            scan.close();
         }
     }
+}
